bbd6e1c8fa5f23e90d5cf1749c59408097e389bd
Dani025a <dani025a@edu.easj.dk>
2024-03-15 01:40:28 +0100

    added uri to .env and .gitignore

--- Authentication-Service/index.js
+++ Authentication-Service/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/auth');
@@ -8,13 +9,13 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 
-mongoose.connect('/auth', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-})
-.then(() => {
-  console.log('Connected to MongoDB');
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-})
-.catch(err => console.error('Error connecting to MongoDB:', err));
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+  })
+  .catch(error => {
+    console.error('Error connecting to MongoDB:', error);
+  });
+
